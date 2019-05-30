0fb9e372d75fc8864619ba0ef5a0c9cfd5a50799
marthan <humberto.marthan@outlook.com>
2019-05-30 07:06:42 -0300

    Upload de Foto Local para Comparação

--- README.md
+++ README.md
@@ -3,34 +3,3 @@ Atentar-se ao link ((https://www.customvision.ai))
 Na sua versão free só pode ter dois projetos ao mesmo tempo cada vez que sobe pelo mvn compile exec:java, gera um novo training para as imagens da pasta resources/respectivasImagens
 
 
----
-services: cognitive-services, custom-vision
-platforms: java
-author: areddish
----
-
-## Getting Started (Open-Source)
-
-### Pre requisites
-
-- An account at [Custom Vision Service](https://www.customvision.ai)
-- Maven
-
-### Quickstart
-
-To get these samples running locally, simply get the pre-requisites above, then:
-
-1. git clone https://github.com/Azure-Samples/cognitive-services-java-sdk-samples.git
-2. cd cognitive-services-java-sdk-samples/Vision/CustomVision
-3. set env variable AZURE_CUSTOMVISION_TRAINING_API_KEY to your cognitive services TRAINING API key.
-4. set env variable AZURE_CUSTOMVISION_PREDICTION_API_KEY to your cognitive services PREDICTION API key.
-5. mvn compile exec:java
-
-## More information ##
-
-[http://azure.com/java](http://azure.com/java)
-
-If you don't have a Microsoft Azure subscription you can get a FREE trial account [here](http://go.microsoft.com/fwlink/?LinkId=330212)
-
----
-
--- src/main/java/br/com/customvision/bean/view/UploadBean.java
+++ src/main/java/br/com/customvision/bean/view/UploadBean.java
@@ -34,11 +34,13 @@ public class UploadBean {
 
 	private UploadedFile uploadedFile;
 
-	public void upload() {
+/*Faz o Upload da Foto para a pasta de escolha... No caso a pasta Test dentro do projeto*/
+public void upload() {
 		try {
-			File file = new File(
-					"C:\\Users\\Humberto\\workspace-tcc\\cognitive-services-java-sdk-samples\\Vision\\CustomVision\\src\\main\\resources\\Test",
-					uploadedFile.getFileName());
+			String caminho = 
+			"C:\\Users\\Humberto\\workspace-tcc\\cognitive-services-java-sdk-samples\\Vision\\CustomVision\\src\\main\\resources\\Test";
+			
+			File file = new File(caminho,uploadedFile.getFileName());
 
 			OutputStream out = new FileOutputStream(file);
 			out.write(uploadedFile.getContents());
@@ -53,9 +55,10 @@ public class UploadBean {
 
 	}
 
-	private static byte[] GetImage(String folder, String fileName) {
+/*Retorna o local e nome da imagem a ser pego*/
+private static byte[] GetImage(String folder, String filename) {
 		try {
-			return ByteStreams.toByteArray(CustomVisionSamples.class.getResourceAsStream(folder + "/" + fileName));
+			return ByteStreams.toByteArray(CustomVisionSamples.class.getResourceAsStream(folder + "/" + filename));
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
@@ -63,9 +66,10 @@ public class UploadBean {
 		return null;
 	}
 
-	private void AddImageToProject(Trainings trainer, Project project, String fileName, byte[] contents, UUID tag,
+/*Adiciona as Imagens ao treinamento*/
+private void AddImageToProject(Trainings trainer, Project project, String fileName, byte[] contents, UUID tag,
 			double[] regionValues) {
-		System.out.println("Adding image: " + fileName);
+		System.out.println("Adicionando imagem: " + fileName);
 		ImageFileCreateEntry file = new ImageFileCreateEntry().withName(fileName).withContents(contents);
 
 		ImageFileCreateBatch batch = new ImageFileCreateBatch().withImages(Collections.singletonList(file));
@@ -85,8 +89,7 @@ public class UploadBean {
 	}
 
 
-
-	public void runSample() {
+public void testar() {
 		
 			try {
 				// =============================================================
@@ -96,25 +99,36 @@ public class UploadBean {
 				
 				final String predictionApiKey = "13aaabd699d8470387f675dfb0671fc7";
 				
-
 				TrainingApi trainClient = CustomVisionTrainingManager.authenticate(trainingApiKey);
 				PredictionEndpoint predictClient = CustomVisionPredictionManager.authenticate(predictionApiKey);
+				// =============================================================
+			
 
-				
-				//ImageClassification_Sample(trainClient, predictClient);
-				
 				System.out.println("Classificação de Cor de olhos");
 				Trainings trainer = trainClient.trainings();
 
+
+			  /*String name = "7d190085-1232-44d6-80e3-3bb85945315f";
+				String source = name;
+				byte[] bytes = source.getBytes("UTF-8");
+				UUID projeto = UUID.nameUUIDFromBytes(bytes);
+				Project project = trainer.getProject(projeto);*/
+				
 				System.out.println("Projeto está sendo criado...");
-				//Project project = trainer.getProject(e958b2c5-5785-4f01-bd83-fe2a6c5771c0);
+				
 				Project project = trainer.createProject().withName("Projeto Diferença de cor de olhos").execute();
+		
+				//azul tag
+				Tag azulTag = trainer.createTag()
+						.withProjectId(project.id())
+						.withName("Azul")
+						.execute();
 
-				// create azul tag
-				Tag azulTag = trainer.createTag().withProjectId(project.id()).withName("Azul").execute();
-
-				// create castanho tag
-				Tag castanhoTag = trainer.createTag().withProjectId(project.id()).withName("Castanho").execute();
+				//castanho tag
+				Tag castanhoTag = trainer.createTag()
+						.withProjectId(project.id())
+						.withName("Castanho")
+						.execute();
 
 				for (int i = 1; i <= 10; i++) {
 					String fileName = "cast_" + i + ".jpg";
@@ -128,23 +142,24 @@ public class UploadBean {
 					AddImageToProject(trainer, project, fileName, contents, azulTag.id(), null);
 				}
 
-				System.out.println("Training...");
+				System.out.println("Treinando Imagens Adicionadas......");
 				Iteration iteration = trainer.trainProject(project.id());
 
 				while (iteration.status().equals("Training")) {
-					System.out.println("Training Status: " + iteration.status());
+					System.out.println("Status do Treino: " + iteration.status());
+					
 					Thread.sleep(1000);
 					iteration = trainer.getIteration(project.id(), iteration.id());
 				}
-				System.out.println("Training Status: " + iteration.status());
+				System.out.println("Status do Treino:	 " + iteration.status());
 				trainer.updateIteration(project.id(), iteration.id(), iteration.withIsDefault(true));
-
-				// load test image
-				byte[] testImage = GetImage("/Test", "test_olhoCastanho.jpg");
-
-				//byte[] testImage = getUploadedFile().getContents();
-
-				// predict
+				
+				String fileName = getUploadedFile().getFileName();
+			
+				// carrega imagem de teste
+				byte[] testImage = GetImage("/Test", fileName);
+				
+				//Predição
 				ImagePrediction results = predictClient.predictions().predictImage().withProjectId(project.id())
 						.withImageData(testImage).execute();
 
@@ -160,70 +175,11 @@ public class UploadBean {
 			}
 	}
 
-	public void ImageClassification_Sample(TrainingApi trainClient, PredictionEndpoint predictor) {
-		try {
-			System.out.println("Classificação de Cor de olhos");
-			Trainings trainer = trainClient.trainings();
-
-			System.out.println("Projeto está sendo criado...");
-			Project project = trainer.createProject().withName("Projeto Diferença de cor de olhos").execute();
-
-			// create azul tag
-			Tag azulTag = trainer.createTag().withProjectId(project.id()).withName("Azul").execute();
-
-			// create castanho tag
-			Tag castanhoTag = trainer.createTag().withProjectId(project.id()).withName("Castanho").execute();
-
-			for (int i = 1; i <= 10; i++) {
-				String fileName = "cast_" + i + ".jpg";
-				byte[] contents = GetImage("/Castanho", fileName);
-				AddImageToProject(trainer, project, fileName, contents, castanhoTag.id(), null);
-			}
-
-			for (int i = 1; i <= 10; i++) {
-				String fileName = "azul_" + i + ".jpg";
-				byte[] contents = GetImage("/Azul", fileName);
-				AddImageToProject(trainer, project, fileName, contents, azulTag.id(), null);
-			}
-
-			System.out.println("Training...");
-			Iteration iteration = trainer.trainProject(project.id());
-
-			while (iteration.status().equals("Training")) {
-				System.out.println("Training Status: " + iteration.status());
-				Thread.sleep(1000);
-				iteration = trainer.getIteration(project.id(), iteration.id());
-			}
-			System.out.println("Training Status: " + iteration.status());
-			trainer.updateIteration(project.id(), iteration.id(), iteration.withIsDefault(true));
-
-			// load test image
-			 byte[] testImage = GetImage("/Test", "test_olhoCastanho.jpg");
-
-			//byte[] testImage = getUploadedFile().getContents();
-
-			// predict
-			ImagePrediction results = predictor.predictions().predictImage().withProjectId(project.id())
-					.withImageData(testImage).execute();
-
-			for (Prediction prediction : results.predictions()) {
-				System.out.println(
-						String.format("\t%s: %.2f%%", 
-								prediction.tagName(), 
-								prediction.probability() * 100.0f));
-
-			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		}
-	}
-
-	public UploadedFile getUploadedFile() {
+public UploadedFile getUploadedFile() {
 		return uploadedFile;
 	}
 
-	public void setUploadedFile(UploadedFile uploadedFile) {
+public void setUploadedFile(UploadedFile uploadedFile) {
 		this.uploadedFile = uploadedFile;
 	}
 
